256667a92fd4985b56fe9414ec40caf6037c3cc1
Rautc252 <rautc252@gmail.com>
2024-11-16 12:17:42 +0530

    running project

---
+++ Documents/workspace-spring-tool-suite-4-4.26.0.RELEASE/SpringBootRegistrationandLogin-1/src/test/java/com/SpringBootRegistrationandLogin1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRegistrationandLogin1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
